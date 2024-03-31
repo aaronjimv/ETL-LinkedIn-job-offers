0eb955813a8569ef77c7a7acc20d426728d44015
Aaron Jimenez <aaronjimv@gmail.com>
2024-03-31 15:13:35 -0800

    task_logs: chage dates

--- src/tasks/task_logs.py
+++ src/tasks/task_logs.py
@@ -4,7 +4,8 @@ from datetime import datetime
 @task(name="LOG EXECUTION")
 async def task_logs(num_offers_extracted, timestamp, error):
    log_entry = {
-      'timestamp': timestamp,
+      'date': timestamp.strftime("%Y-%m-%d"),
+      'hour': timestamp.strftime("%H:%M"),
       'number_of_offers_extracted': num_offers_extracted,
       'error': error
    }
