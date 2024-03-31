209ed1dd43ff79233c2eb31b293ff0c5399f6a48
Aaron Jimenez <aaronjimv@gmail.com>
2024-03-31 15:24:04 -0800

    task_logs: store logs in csv

--- src/tasks/task_logs.py
+++ src/tasks/task_logs.py
@@ -1,13 +1,25 @@
 from prefect import task
 from datetime import datetime
+import csv
 
 @task(name="LOG EXECUTION")
 async def task_logs(num_offers_extracted, timestamp, error):
    log_entry = {
       'date': timestamp.strftime("%Y-%m-%d"),
       'hour': timestamp.strftime("%H:%M"),
-      'number_of_offers_extracted': num_offers_extracted,
+      'offers_extracted': num_offers_extracted,
       'error': error
    }
 
-   print(f"Log entry: {log_entry}")
+   with open('data/execution_logs.csv', mode='a', newline='') as file:
+      writer = csv.DictWriter(file, fieldnames=[
+         'date',
+         'hour',
+         'offers_extracted',
+         'error'
+      ])
+
+      if file.tell() == 0:
+         writer.writeheader()
+      
+      writer.writerow(log_entry)
